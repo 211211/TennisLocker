baa24bd27e9772f7860fd474006cd293b899cc1f
211211 <nhquan307@outlook.com>
2019-01-03 23:15:52 +0700

    Moving helper from TennisLockerInternalPortal project

---
+++ src/helpers/colorDashboardSelect.js
@@ -0,0 +1,158 @@
+import { find } from "lodash"
+
+class AddColorButtons {
+  colorButtons = response => {
+    const arrButtons = [];
+    Object.keys(response.data).forEach(item => {
+      const str = item
+        .replace(/Count/g, "")
+        .replace(/([A-Z]+)/g, " $1")
+        .replace(/([A-Z][a-z])/g, "$1");
+      switch (item) {
+        case "groupCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "playerCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "parentCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "dailyEvalCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#1c65af",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "fitnessTestCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#e6d21f",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "singlesPMCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#E50404",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "doublesPMCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#E50404",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "goalsCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#624096",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "eventsCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#ea8637",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "attendanceCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#8acb3f",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "tournamentCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#aed7ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "courtsCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "pushNotificationCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#f7ab6f",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "coachesCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "sessionCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#65b2ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "filesCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#ef35ff",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        case "playerCalendarCount":
+          arrButtons.push({
+            name: str.charAt(0).toUpperCase() + str.slice(1),
+            color: "#ff8dbf",
+            count: response.data[item],
+            activeFlag: false
+          });
+          break;
+        default:
+          console.log("new button need add color!!!", item);
+      }
+    });
+    response.data = arrButtons;
+    return response;
+  };
+}
+
+const addColorButtons = new AddColorButtons();
+export default addColorButtons;
---
+++ src/helpers/navTabs.js
@@ -0,0 +1,109 @@
+import Dashboard from "../containers/NavSections/Dashboard";
+import Users from "../containers/NavSections/Users";
+import Players from "../containers/NavSections/Players";
+import Coaches from "../containers/NavSections/Coaches";
+import Parents from "../containers/NavSections/Parents";
+import Groups from "../containers/NavSections/Groups";
+import Events from "../containers/NavSections/Events";
+import FitnessTest from "../containers/NavSections/FitnessTest";
+import PracticeMatches from "../containers/NavSections/PracticeMatches";
+import Goals from "../containers/NavSections/Goals";
+import Files from "../containers/NavSections/Files";
+import PushNotifications from "../containers/NavSections/PushNotifications";
+import About from "../containers/NavSections/About";
+
+import dashboard from "../assets/images/icons/dashboard.svg";
+import user from "../assets/images/icons/user.svg";
+import players from "../assets/images/icons/playear.svg";
+import coaches from "../assets/images/icons/coaches.svg";
+import groups from "../assets/images/icons/groups.svg";
+import events from "../assets/images/icons/events.svg";
+import goals from "../assets/images/icons/goal.svg";
+import pushNotifications from "../assets/images/icons/notif.svg";
+import about from "../assets/images/icons/about.svg";
+import files from "../assets/images/icons/folder.svg";
+import practiceMatches from "../assets/images/icons/tennis-ball.svg";
+import fitnessTest from "../assets/images/icons/tasks.svg";
+import parents from "../assets/images/icons/parents.svg";
+
+const navigationTabs = [
+  {
+    urlName: "/dashboard",
+    iconSvg: dashboard,
+    name: "Dashboard",
+    comFile: Dashboard
+  },
+  {
+    urlName: "/users",
+    iconSvg: user,
+    name: "Users",
+    comFile: Users
+  },
+  {
+    urlName: "/players",
+    iconSvg: players,
+    name: "Players",
+    comFile: Players
+  },
+  {
+    urlName: "/coaches",
+    iconSvg: coaches,
+    name: "Coaches",
+    comFile: Coaches
+  },
+  {
+    urlName: "/parents",
+    iconSvg: parents,
+    name: "Parents",
+    comFile: Parents
+  },
+  {
+    urlName: "/groups",
+    iconSvg: groups,
+    name: "Groups",
+    comFile: Groups
+  },
+  {
+    urlName: "/events",
+    iconSvg: events,
+    name: "Events",
+    comFile: Events
+  },
+  {
+    urlName: "/fitnessTest",
+    iconSvg: fitnessTest,
+    name: "Fitness Test",
+    comFile: FitnessTest
+  },
+  {
+    urlName: "/practiceMatches",
+    iconSvg: practiceMatches,
+    name: "Practice Matches",
+    comFile: PracticeMatches
+  },
+  {
+    urlName: "/goals",
+    iconSvg: goals,
+    name: "Goals",
+    comFile: Goals
+  },
+  {
+    urlName: "/files",
+    iconSvg: files,
+    name: "Files",
+    comFile: Files
+  },
+  {
+    urlName: "/pushNotifications",
+    iconSvg: pushNotifications,
+    name: "Push Notifications",
+    comFile: PushNotifications
+  },
+  {
+    urlName: "/about",
+    iconSvg: about,
+    name: "About",
+    comFile: About
+  }
+];
+export default { navigationTabs };
