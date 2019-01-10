f4a03006d3e333632b58771e56556c98757dc293
211211 <nhquan307@outlook.com>
2019-01-10 18:01:18 +0700

    Remove fake data for dashboard and add redux dev tool

--- src/app/routes/dashboard/index.js
+++ src/app/routes/dashboard/index.js
@@ -3,64 +3,65 @@ import "./dashboard.scss";
 import DashboardInfo from "./DashboardInfo";
 import DashboardButtons from "./DashboardButtons";
 import DashboardChart from "./DashboardChart";
-import data from './data'
 import { connect } from "react-redux";
 import {
-  getFacilitiesToday,
-  getFacilitiesSelectDate
+    getFacilitiesToday,
+    getFacilitiesSelectDate
 } from "../../../actions/Facility";
 
 const mapStateToProps = ({ facilityFilter }) => ({
-  facilityFilter
+    facilityFilter
 });
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const facility = this.props.facilityFilter;
-    const facilityId = facility.facilityActive.id;
-    const flag = facility.flagFilter;
-    const activeFacility = facility.activeDateSelect;
-    const { facilityFilter: { flagFilter } } = this.props;
-
-    if (facilityId !== null) {
-      if (flag) {
-        if (facility.activeDateSelect) {
-          this.props.getFacilitiesSelectDate(
-            facilityId,
-            activeFacility.startDay,
-            activeFacility.endDay
-          );
-        } else {
-          this.props.getFacilitiesToday(facilityId);
-        }
-      }
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
+    render() {
+        const facility = this.props.facilityFilter;
+        const facilityId = facility.facilityActive.id;
+        const flag = facility.flagFilter;
+        const activeFacility = facility.activeDateSelect;
+        const { facilityFilter: { flagFilter } } = this.props;
 
-    return (
-      <div className="dashboard">
-        <div className="dashboard_blog">
-          <div className="dashboard_blog-buttons">
-            <DashboardButtons facilityBtn={facility.facilityDate} />
-          </div>
-          <DashboardInfo facilityActive={facility.facilityActive} />
-        </div>
-        <p className="features_title chart_title">Usage comparison</p>
-        <div className="dashboard_chart">
-          <DashboardChart />
-        </div>
-      </div>
-    );
-  }
+        if (facilityId !== null) {
+            if (flag) {
+                if (facility.activeDateSelect) {
+                    this.props.getFacilitiesSelectDate(
+                        facilityId,
+                        activeFacility.startDay,
+                        activeFacility.endDay
+                    );
+                } else {
+                    this.props.getFacilitiesToday(facilityId);
+                }
+            }
+        }
+
+        return (
+            <div className="dashboard">
+                {
+                    //   <div className="dashboard_blog">
+                    //     <div className="dashboard_blog-buttons">
+                    //       <DashboardButtons facilityBtn={facility.facilityDate} />
+                    //     </div>
+                    //     <DashboardInfo facilityActive={facility.facilityActive} />
+                    //   </div>
+                    //   <p className="features_title chart_title">Usage comparison</p>
+                    //   <div className="dashboard_chart">
+                    //     <DashboardChart />
+                    //   </div>
+                }
+            </div>
+        );
+    }
 }
 
 export default connect(
-  mapStateToProps,
-  {
-    getFacilitiesToday: id => getFacilitiesToday(id),
-    getFacilitiesSelectDate: (id, startDay, endDay) => getFacilitiesSelectDate(id, startDay, endDay)
-  }
+    mapStateToProps,
+    {
+        getFacilitiesToday: id => getFacilitiesToday(id),
+        getFacilitiesSelectDate: (id, startDay, endDay) => getFacilitiesSelectDate(id, startDay, endDay)
+    }
 )(Dashboard);
--- src/store/index.js
+++ src/store/index.js
@@ -6,23 +6,38 @@ import rootSaga from '../sagas/index';
 import { routerMiddleware } from 'react-router-redux';
 
 
-const history = createHistory ();
-const routeMiddleware = routerMiddleware (history);
-const sagaMiddleware = createSagaMiddleware ();
+const history = createHistory();
+const routeMiddleware = routerMiddleware(history);
+const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware, routeMiddleware];
 
-export default function configureStore (initialState) {
-    const store = createStore (reducers, initialState,
-        compose (applyMiddleware (...middlewares)));
+export default function configureStore(initialState) {
+    const composeEnhancers =
+        typeof window === 'object' &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+                // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+            }) : compose;
 
-    sagaMiddleware.run (rootSaga);
+    const enhancer = composeEnhancers(
+        applyMiddleware(...middlewares),
+        // other store enhancers if any
+    );
+
+    const store = createStore(
+        reducers,
+        initialState,
+        enhancer,
+    );
+
+    sagaMiddleware.run(rootSaga);
 
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
-        module.hot.accept ('../reducers/index', () => {
-            const nextRootReducer = require ('../reducers/index');
-            store.replaceReducer (nextRootReducer);
+        module.hot.accept('../reducers/index', () => {
+            const nextRootReducer = require('../reducers/index');
+            store.replaceReducer(nextRootReducer);
         });
     }
     return store;
