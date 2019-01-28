6a2561ab40f2e798e7a19077225c61dd26001abf
211211 <nhquan307@outlook.com>
2019-01-28 23:11:03 +0700

    External Calendar migration: Add config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
-const hrefUrl = "";
-const baseUrl = "https://api.sportsanalyticsinc.com";
-export default { hrefUrl, baseUrl };
+const hrefUrl = '';
+const token = null; // Ex: 5qUulvNbLKX3ezxNdqdHWA==
+const hecColor = null // Ex: 3f51b5
+const baseUrl = 'https://api.sportsanalyticsinc.com';
+export default { hrefUrl, baseUrl, token, hecColor };
