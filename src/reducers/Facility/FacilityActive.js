bf59b4db1252e3da9da9b2f00d7390d0441f4935
211211 <nhquan307@outlook.com>
2019-02-06 13:42:53 +0700

    Fix bug: chart does not render new numbers when selecting new facility

--- src/reducers/Facility/FacilityActive.js
+++ src/reducers/Facility/FacilityActive.js
@@ -1,6 +1,8 @@
+import isEmpty from 'lodash/isEmpty'
 import {
     ACTIVE_FACILITY_ARRAY,
-    GET_FACILITIES_SELECT_DATE_SUCCESS
+    GET_FACILITIES_SELECT_DATE_SUCCESS,
+    GET_FACILITIES_TODAY_SUCCESS,
 } from '../../constants/ActionTypes';
 
 const initial = {
@@ -18,6 +20,20 @@ const FacilityActive = (state = initial, action) => {
             return {
                 ...state,
                 activeFacilityArray: action.payload.activeFacilityArray
+            };
+        case GET_FACILITIES_TODAY_SUCCESS:
+            let newActiveFacilityArray = []
+            state.activeFacilityArray.forEach((activeFacility) => {
+                newActiveFacilityArray.push(action.payload.facilityDate.find(({ code }) => code === activeFacility.code))
+            })
+
+            if (isEmpty(newActiveFacilityArray)) {
+                return state
+            }
+
+            return {
+                ...state,
+                activeFacilityArray: newActiveFacilityArray
             };
         default:
             return state;
